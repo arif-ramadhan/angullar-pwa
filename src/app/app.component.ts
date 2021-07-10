2d5b3eeb52114678bd13a5df24c4c36b4ef7e335
Rama <macbookpro@Macbooks-MacBook-Pro.local>
2021-07-10 16:12:49 +0700

    angular pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jokes';
+
+  update: boolean = false
+  public joke: any
+
+  constructor(updates: SwUpdate, private data: DataService) {
+    updates.available.subscribe(() => {
+      this.update=true
+    })
+  }
+
+  ngOnInit() {
+    this.data.gimmeJokes().subscribe(res => {
+      this.joke = res
+    })
+  }
 }
